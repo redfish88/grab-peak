a510b86737e656246f21959b9ff2a429d992c956
rklv <weHbkC3K>
2017-03-24 14:51:52 +0800

    Initial commit

---
+++ src/content/article_category.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+
+def get_category():
+    category_url = "http://www.ruanyifeng.com/blog/archives.html"
+    r = requests.get(category_url)
+    # print r.content
+    soup = BeautifulSoup(r.content)
+    print soup.prettify()
+    # tag_list = soup.select('.module-list-item')
+    for tag in soup.select('#beta-inner li'):
+        # print tag.get_text()
+        print tag.a
+        print tag.a.get('href')
+
+
+
+
+if __name__ == '__main__':
+    get_category()
