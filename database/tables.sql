74461d4179d57e646a16623487144cc3db63766f
rklv <rklv@corp.netease.com>
2017-07-25 19:56:39 +0800

    标签，文章内容抓取

--- database/tables.sql
+++ database/tables.sql
@@ -2,5 +2,28 @@ create table blog_user(id INTEGER PRIMARY KEY auto_increment,name VARCHAR (100),
 
 insert into blog_user(name,description) values('阮一峰的博客','测试');
 
-create table blog_category(id INTEGER PRIMARY KEY auto_increment,href VARCHAR (100), tag_name VARCHAR (100))
+CREATE TABLE `blog_category` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`href` VARCHAR(100) NULL DEFAULT NULL,
+	`tag_name` VARCHAR(100) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+
+CREATE TABLE `article` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`tag_id` INT(11) NOT NULL DEFAULT '0',
+	`link` VARCHAR(500) NOT NULL DEFAULT '',
+	`title` VARCHAR(500) NOT NULL DEFAULT '',
+	`content` TEXT NOT NULL,
+	`update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
 
